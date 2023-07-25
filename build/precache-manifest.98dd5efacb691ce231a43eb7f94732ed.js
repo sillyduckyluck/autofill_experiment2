0d84a4db41969916035f6fb28039714ac4f2d872
sasha <sillyduckyluck@gmail.com>
2023-07-25 15:13:44 +0200

    wtf

---
+++ build/precache-manifest.98dd5efacb691ce231a43eb7f94732ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "956559382c4fca4d39d0",
+    "url": "/static/css/main.f3f708a2.chunk.css"
+  },
+  {
+    "revision": "956559382c4fca4d39d0",
+    "url": "/static/js/main.95655938.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "bd5440dfbcfe3e971529",
+    "url": "/static/js/runtime~main.bd5440df.js"
+  },
+  {
+    "revision": "1544de55c6a007f9c64f05dbc97d4dd9",
+    "url": "/index.html"
+  }
+];
