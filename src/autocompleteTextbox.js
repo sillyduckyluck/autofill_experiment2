08393ad51379593c4f1b4e56db2fb3080c20c7fb
sasha <sillyduckyluck@gmail.com>
2023-08-31 01:33:26 +0200

    env variable set

--- src/autocompleteTextbox.js
+++ src/autocompleteTextbox.js
@@ -22,6 +22,7 @@ function AutocompleteTextbox() {
 
     const prompt = "Imagine you are an autocomplete feature tailored to provide suggestions with extraverted connotations. Your goal is to assist the user in completing their sentences by offering contextually relevant endings that highlight their openness to experience in a social and outgoing manner. As you generate suggestions, lean towards choices that emphasize interactions, social events, group activities, and enthusiastic participation. Encourage the user to express their extroverted qualities and engage in experiences that involve others. An autocomplete feature finishes the sentece without suggesting what came before, so ONLY FINISH THE SENTENCE, without repeating the part I am about to tell you. How would you COMPLETE the following sentence? (DO NOT REPEAT THE BEGINNING)"
     const apiKey = process.env.GPT_KEY;
+    console.log(apiKey)
     const endpoint = 'https://api.openai.com/v1/chat/completions';
 
     console.log(prompt)
